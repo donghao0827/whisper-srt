b9af02f9bc9334d25f493176e9e99631524179d9
donghao058 <donghao058@ke.com>
2025-03-31 18:27:56 +0800

    重置项目

--- whispersubtitle/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-WhisperSubtitle: 基于OpenAI Whisper的多功能字幕生成工具
-"""
-
-__version__ = '1.0.0'
-__author__ = 'WhisperSubtitle Team' 
--- whispersubtitle/core/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-核心字幕生成功能
-"""
-
-from .generator import extract_audio, generate_subtitles, format_timestamp, export_json 
